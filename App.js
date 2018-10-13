6e74efb340486e6aff48d8ee3a4c6f867c1c802e
Arley HR <arley.hr@hotmail.com>
2018-10-13 12:13:26 -0500

    loaded project fonts

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo'
 
 export default class App extends React.Component {
+  state = {
+    isAppReady: false
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'cubano-regular': require('./assets/fonts/cubano-regular.ttf'),
+      'bariol-regular': require('./assets/fonts/bariol-regular.ttf'),
+      'bariol-bold': require('./assets/fonts/bariol-bold.ttf')
+    });
+    this.setState({ isAppReady: true });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {
+          this.state.isAppReady && (
+            <Text style={styles.text}>Hello Delivery!</Text>
+          )
+        }
       </View>
     );
   }
@@ -18,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'cubano-regular',
+  }
 });
